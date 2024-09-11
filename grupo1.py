e064ef3c85ac7b835d1658fa1f228ac8c9398aba
agonzalezmonin <agonzalezmonin@uade.edu.ar>
2024-09-11 10:57:12 -0300

    Les dejo la primera función, cambienle el nombre a la 2da variable si así lo prefieren.

--- grupo1.py
+++ grupo1.py
@@ -1,3 +1,22 @@
-print ("hola mundo")
-print ('auxilio')
-print ("buenas noches")
+import random
+
+def lista_fechas_random():
+    fechas = []
+    variable = (0, 30)
+    for i in range(10):
+        hora = random.randint(8, 22)
+        minutos = random.choice(variable)
+        mes = random.randint(1, 12)
+
+        if mes == 2:
+            dia = random.randint(1, 28)
+        elif mes in [1, 3, 5, 7, 8, 10, 12]:
+            dia = random.randint(1, 31)
+        else:
+            dia = random.randint(1, 30)    
+        fecha = f"{dia}/{mes}    {hora:02}:{minutos:02}"
+        fechas.append(fecha)
+
+    return fechas
+
+print(lista_fechas_random())
