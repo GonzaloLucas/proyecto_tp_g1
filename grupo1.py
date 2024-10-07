c42ead11d7f26034e0232bb85dd9302f419b7a0e
gonzalo_lucas_lane <glane@uade.edu.ar>
2024-10-07 14:57:04 -0300

    complejización de base de datos (cosas pendientes) y implementacion de excepciones

--- grupo1.py
+++ grupo1.py
@@ -1,10 +1,13 @@
 import random
 
-def lista_fechas_random():
-    fechas = []
+def DiccEspecialidades():
+    fechas_1 = []
+    fechas_2 = []
     variable = (0, 30)
+    
     for i in range(10):
-        hora = random.randint(8, 22)
+        hora_1 = random.randint(7, 14)
+        hora_2 = random.randint(15, 22)
         minutos = random.choice(variable)
         mes = random.randint(1, 12)
 
@@ -14,34 +17,55 @@ def lista_fechas_random():
             dia = random.randint(1, 31)
         else:
             dia = random.randint(1, 30)    
-        fecha = f"{dia}/{mes}    {hora:02}:{minutos:02}"
-        fechas.append(fecha)
-    return fechas
+        fecha_1 = f"{dia}/{mes}    {hora_1:02}:{minutos:02}"
+        fecha_2 = f"{dia}/{mes}    {hora_2:02}:{minutos:02}"
+        fechas_1.append(fecha_1)
+        fechas_2.append(fecha_2)
+        
+    especialidades = ['Cardiologia', 'Dermatologia', 'Cirugia General', 'Endocrinologia', 'Gastroenterologia',
+                     'Ginecologia', 'Neumonologia', 'Neurocirugia', 'Oftalmologia', 'Pediatria',
+                     'Psiquiatria', 'Traumatologia']
 
-def DiccDoctores():
-    lista_de_fechas= lista_fechas_random()
     doctores = [
-    "Dr. Juan Pérez", "Dr. María González", "Dr. Carlos Ramírez", "Dr. Ana Martínez", "Dr. José Hernández",
-    "Dr. Laura López", "Dr. Luis Fernández", "Dr. Carmen Ortiz", "Dr. Manuel García", "Dr. Elena Díaz",
-    "Dr. Alberto Sánchez", "Dr. Patricia Torres", "Dr. Jorge Romero", "Dr. Lucía Castro", "Dr. Andrés Moreno",
-    "Dr. Adriana Reyes", "Dr. Antonio Jiménez", "Dr. Beatriz Vargas", "Dr. Ricardo Soto", "Dr. Claudia Peña",
-    "Dr. Alejandro Silva", "Dr. Verónica Flores", "Dr. Eduardo Muñoz", "Dr. Gabriela Castillo", "Dr. Francisco Paredes",
-    "Dr. Daniela Mendoza", "Dr. Raúl Rivas", "Dr. Teresa Serrano", "Dr. Sergio Aguilar", "Dr. Pilar Ponce"
-]
-    random.shuffle(doctores)
-    dicc_doc = {doctor: lista_de_fechas for doctor in doctores[:5]}
-    return dicc_doc
-
-def DiccEspecialidades():
-    listadoctor = DiccDoctores()
-    especialidades = [
-        'Cardiologia', 'Dermatologia', 'Cirugia General', 'Endocrinologia',
-        'Gastroenterologia', 'Ginecologia', 'Neumonologia', 'Neurocirugia',
-        'Oftalmologia', 'Pediatria', 'Psiquiatria', 'Traumatologia'
+        "Dr. Juan Pérez", "Dr. María González", "Dr. Carlos Ramírez", "Dr. Ana Martínez", "Dr. José Hernández",
+        "Dr. Laura López", "Dr. Luis Fernández", "Dr. Carmen Ortiz", "Dr. Manuel García", "Dr. Elena Díaz",
+        "Dr. Alberto Sánchez", "Dr. Patricia Torres", "Dr. Jorge Romero", "Dr. Lucía Castro", "Dr. Andrés Moreno",
+        "Dr. Adriana Reyes", "Dr. Antonio Jiménez", "Dr. Beatriz Vargas", "Dr. Ricardo Soto", "Dr. Claudia Peña",
+        "Dr. Alejandro Silva", "Dr. Verónica Flores", "Dr. Eduardo Muñoz", "Dr. Gabriela Castillo", "Dr. Francisco Paredes",
+        "Dr. Daniela Mendoza", "Dr. Raúl Rivas", "Dr. Teresa Serrano", "Dr. Sergio Aguilar", "Dr. Pilar Ponce",
+        "Dr. Diego Núñez", "Dr. Valentina Morales", "Dr. Ricardo Fernández", "Dr. Gloria Vega", "Dr. Roberto Medina",
+        "Dr. Susana Castro", "Dr. Francisco Núñez", "Dr. Elisa Serrano", "Dr. Martín Muñoz", "Dr. Camila Rodríguez",
+        "Dr. Javier Pérez", "Dr. Liliana Blanco", "Dr. Emilio Torres", "Dr. Natalia Salazar", "Dr. Miguel Reyes",
+        "Dr. Esteban Castillo", "Dr. Catalina Silva", "Dr. Héctor López", "Dr. Silvia Ramos", "Dr. Sergio Gómez",
+        "Dr. Ingrid Ortega", "Dr. Óscar Benítez", "Dr. Iván Aguilera", "Dr. Marcos Gutiérrez", "Dr. Gabriela Acosta",
+        "Dr. Claudia Guzmán", "Dr. Ramón Herrera", "Dr. Andrés Flores", "Dr. Mónica Gallardo", "Dr. Estefanía Ruiz"
     ]
+    
+    random.shuffle(doctores)
+    
+    especialidades_1 = ['Cardiologia', 'Dermatologia', 'Cirugia General', 'Endocrinologia', 'Gastroenterologia',
+                     'Ginecologia']
+    especialidades_2 = ['Neumonologia', 'Neurocirugia', 'Oftalmologia', 'Pediatria',
+                     'Psiquiatria', 'Traumatologia']
 
-    dicc_especialidades = {i: listadoctor for i in especialidades[:12]}
+    dicc_especialidades = {}
+
+    for especialidad in especialidades:
+        if len(doctores) < 5:
+            break
+        
+        doctores_asignados = doctores[:5]
+        doctores = doctores[5:]
+        
+        if especialidad in especialidades_1:
+            dicc_doc = {doctor: fechas_1 for doctor in doctores_asignados}
+        elif especialidad in especialidades_2:
+            dicc_doc = {doctor: fechas_2 for doctor in doctores_asignados}
+        
+        dicc_especialidades[especialidad] = dicc_doc
+    
     return dicc_especialidades
+
 
 def DatosUsuario():
     dni= input("Ingrese su DNI: ")
@@ -96,57 +120,77 @@ matriz_especialidad = [[lista_complementaria_especialidad[i], lista_especialidad
 
 while True:
     if decision == "y":
+        print("\n" + "=" * 40 + "\n")
+        print("Especialidades")
+        print("-" * 40)
         for i in range(len(lista_especialidad)):
             print(f"{lista_complementaria_especialidad[i]} {lista_especialidad[i]}")
-
+            
+        print("\n" + "=" * 40 + "\n")
         print()
         
         while True:
-            desicion1 = int(input("Ingrese el número correspondiente a la especialidad que desee: "))
-            if 1 <= desicion1 <= len(matriz_especialidad):
-                especialidad_usuario = lista_especialidad[desicion1 - 1]
-                print(f"Has seleccionado: {especialidad_usuario}")
-                break 
-            else:
-                print("Valor fuera de rango. Por favor, ingrese un número válido.")
+            try:
+                desicion1 = int(input("Ingrese el número correspondiente a la especialidad que desee: "))
+                if 1 <= desicion1 <= len(matriz_especialidad):
+                    especialidad_usuario = lista_especialidad[desicion1 - 1]
+                    print(f"Has seleccionado: {especialidad_usuario}")
+                    break
+                else:
+                    print("Valor fuera de rango. Por favor, ingrese un número válido.")
+            except ValueError:
+                print("Entrada no válida. Por favor, ingrese un número.")
+
 
         doctor = especialidades.get(especialidad_usuario)
         lista_doctor = list(doctor)
         lista_complementaria_doctor = [i+1 for i in range(len(lista_doctor))]
         matriz_doctor = [[lista_complementaria_doctor[i], lista_doctor[i]] for i in range(len(lista_doctor))]
+        print("\n" + "=" * 40 + "\n")
+        print(f"Doctores correspondiente a la especialidad: {especialidad_usuario}")
+        print("-" * 40)
         for i in range(len(lista_doctor)):
             print(f"{lista_complementaria_doctor[i]} {lista_doctor[i]}")
+        print("\n" + "=" * 40 + "\n")
         print()
 
         while True:
-            desicion2 = int(input("Ingrese el número correspondiente al doctor que desee: "))
-            if 1 <= desicion2 <= len(matriz_doctor):
-                doctor_usuario = lista_doctor[desicion2 - 1]
-                break
-            else:
-                print("Valor fuera de rango. Por favor, ingrese un número válido.")
+            try:
+                desicion2 = int(input("Ingrese el número correspondiente al doctor que desee: "))
+                if 1 <= desicion2 <= len(matriz_doctor):
+                    doctor_usuario = lista_doctor[desicion2 - 1]
+                    print(f"Has seleccionado: {doctor_usuario}")
+                    break
+                else:
+                    print("Valor fuera de rango. Por favor, ingrese un número válido.")
+            except ValueError:
+                print("Entrada no válida. Por favor, ingrese un número.")
 
         horario = doctor.get(doctor_usuario)
         lista_horario = list(horario)
         lista_complementaria_horario = [i+1 for i in range(len(lista_horario))]
         matriz_horario = [[lista_complementaria_horario[i], lista_horario[i]] for i in range(len(lista_horario))]
+        print("\n" + "=" * 40 + "\n")
+        print(f"Horarios correspondientes al Dr/Dra: {doctor_usuario}")
+        print("-" * 40)
         for i in range(len(lista_horario)):
             print(f"{lista_complementaria_horario[i]} {lista_horario[i]}")
+        print("\n" + "=" * 40 + "\n")
         print()
 
         while True:
-            desicion3 = int(input("Ingrese el número correspondiente al horario que desee: "))
-            if 1 <= desicion3 <= len(matriz_horario):
-                horario_usuario = lista_horario[desicion3 - 1]
-                break
-            else:
-                print("Valor fuera de rango. Por favor, ingrese un número válido.")
+            try:
+                desicion3 = int(input("Ingrese el número correspondiente a la especialidad que desee: "))
+                if 1 <= desicion3 <= len(matriz_horario):
+                    horario_usuario = lista_horario[desicion3 - 1]
+                    print(f"Has seleccionado: {horario_usuario}")
+                    break
+                else:
+                    print("Valor fuera de rango. Por favor, ingrese un número válido.")
+            except ValueError:
+                print("Entrada no válida. Por favor, ingrese un número.")
 
         ImpresionTurno(especialidad_usuario, doctor_usuario, horario_usuario, usuario)
         break
-
     else:
         break
-
-
-
